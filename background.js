e6c5700db7e5214b94fb499d45bbbe2143d3be3f
DJMaest <sazimi@ualberta.ca>
2022-01-24 03:27:43 -0700

    fix

--- background.js
+++ background.js
@@ -1,30 +1,31 @@
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//   // read changeInfo data and do something with it
-//   // like send the new url to contentscripts.js
-//   if (changeInfo.url) {
-//     chrome.tabs.sendMessage(tabId, {
-//       message: "hello!",
-//       url: changeInfo.url,
-//     });
-//   }
-// });
 var isMasterURL = true;
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if(details.frameId === 0) {
-      // Fires only when details.url === currentTab.url
-      console.log(details);
-      chrome.tabs.get(details.tabId, function(tab) {
-          if(tab.url === details.url) 
-          {
-            isMasterURL && chrome.tabs.sendMessage(details.tabId, {
-              message: "hello!",
-              url: tab.url,
-            });
-              console.log("onHistoryStateUpdated");
-          }
-      });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // read changeInfo data and do something with it
+  // like send the new url to contentscripts.js
+  if (changeInfo.url) {
+    chrome.tabs.sendMessage(tabId, {
+      message: "hello!",
+      url: changeInfo.url,
+    });
   }
 });
+
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+//   if(details.frameId === 0) {
+//       // Fires only when details.url === currentTab.url
+//       console.log(details);
+//       chrome.tabs.get(details.tabId, function(tab) {
+//           if(tab.url === details.url) 
+//           {
+//             isMasterURL && chrome.tabs.sendMessage(details.tabId, {
+//               message: "hello!",
+//               url: tab.url,
+//             });
+//               console.log("onHistoryStateUpdated");
+//           }
+//       });
+//   }
+// });
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.message == "recievedURL"){
@@ -32,7 +33,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 
     setTimeout(()=>{
       isMasterURL = true;
-    }, 800);
+    }, 500);
   }
    
 });
